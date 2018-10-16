be34a4e88dcc8c332cb9f2bad15aed15ca1e563a
Mustafa <amustafa246@yahoo.com>
2018-10-16 17:49:13 -0500

    final boilerplate setup

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,23 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { TabNavigator } from "react-navigation";
 import store from "./store";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default class App extends React.Component {
+  componentDidMount() {
+    // Is called when the component is mounted on the screen
+  }
+
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen }
+    });
+
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <MainNavigator />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
