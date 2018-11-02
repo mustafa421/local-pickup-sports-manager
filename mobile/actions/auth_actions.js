870b07094e49eb178f524700f2589348ff8d5532
Mustafa <amustafa246@yahoo.com>
2018-11-01 20:54:12 -0500

    begin refactoring fblogin flow

--- mobile/actions/auth_actions.js
+++ mobile/actions/auth_actions.js
@@ -3,6 +3,24 @@ import { Facebook } from "expo";
 import { LOGIN_SUCCESS, LOGIN_FAIL } from "./types";
 
 // Helper methods
+const setupUserData = async (userInfo, token, dispatch) => {
+  // Call backend with newly acquired account information to create account
+  const request = await fetch(
+    "http://local-pickup-sports-manager.herokuapp.com/loginUser",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userInfo),
+      json: true
+    }
+  );
+
+  const userAccountData = await request.json(); // check http status
+  dispatch({ type: LOGIN_SUCCESS, payload: { token, userAccountData } });
+};
+
 const doFacebookLogin = async dispatch => {
   const { type, token } = await Facebook.logInWithReadPermissionsAsync(
     "314718532447126",
@@ -18,31 +36,30 @@ const doFacebookLogin = async dispatch => {
   if (type === "success") {
     try {
       await AsyncStorage.setItem("fb_token", token);
-      dispatch({ type: LOGIN_SUCCESS, payload: token });
+      // dispatch({ type: LOGIN_SUCCESS, payload: token });
       const response = await fetch(
         `https://graph.facebook.com/v3.2/me?fields=id,name,gender,age_range,email&access_token=${token}`
       );
 
       const account = await response.json();
+      console.log("Retrieve account from facebook login: ");
       console.log(account);
 
+      setupUserData(account, token, dispatch);
       // Call backend with newly acquired account information to create account
-      const request = await fetch(
-        "http://local-pickup-sports-manager.herokuapp.com/loginUser",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(account),
-          json: true
-        }
-      );
+      // const request = await fetch(
+      //   "http://local-pickup-sports-manager.herokuapp.com/loginUser",
+      //   {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json"
+      //     },
+      //     body: JSON.stringify(account),
+      //     json: true
+      //   }
+      // );
 
-      console.log((await request.json()).status);
-
-      // AsyncStorage.setItem("account", JSON.stringify(account));
-      // console.log(await response.json());
+      // console.log((await request.json()).status);
     } catch (err) {
       console.log(`Error saving facebook token ${err}`);
     }
@@ -51,17 +68,29 @@ const doFacebookLogin = async dispatch => {
 
 export const fbLogin = () => async dispatch => {
   let token;
-  let account;
   try {
     token = await AsyncStorage.getItem("fb_token");
-    account = JSON.parse(await AsyncStorage.getItem("account"));
   } catch (err) {
     console.log(`Error getting data from storage: ${err}`);
   }
+
+  // TODO - Check for expired tokens
+  // If the user already has a token
   if (token) {
-    // Dispatch succesful login
-    console.log(`${account.name} logged in`);
-    dispatch({ type: LOGIN_SUCCESS, payload: token });
+    const response = await fetch(
+      `https://graph.facebook.com/v3.2/me?fields=id,name,gender,age_range,email&access_token=${token}`
+    );
+
+    const account = await response.json(); // should be in try-catch
+    const statusCode = response.status;
+
+    if (statusCode !== 200) {
+      // Do something
+      dispatch({ type: LOGIN_FAIL });
+      console.log(`Server responded ${statusCode}`);
+    } else {
+      setupUserData(account, token, dispatch);
+    }
   } else {
     // Open fb login
     doFacebookLogin(dispatch);
