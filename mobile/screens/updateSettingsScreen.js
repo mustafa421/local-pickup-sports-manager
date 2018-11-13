0f8926dc25ab68793fd183c4fd872b19794db2f2
Max <mglickman13@gmail.com>
2018-11-13 15:39:32 -0600

    Adding preferred sports options

--- mobile/screens/updateSettingsScreen.js
+++ mobile/screens/updateSettingsScreen.js
@@ -5,7 +5,8 @@ import {
   ScrollView,
   Platform,
   Text,
-  TextInput
+  TextInput,
+  Switch
 } from "react-native";
 import { Button } from "react-native-elements";
 // import {Text, Platform } from "react-native";
@@ -34,7 +35,13 @@ class updateSettingsScreen extends Component {
     }
   });
 
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
+    const { phone, football, email } = this.state;
     return (
       <ScrollView>
         <View style={styles.container}>
@@ -45,21 +52,34 @@ class updateSettingsScreen extends Component {
               color: "grey"
             }}
           >
-            Preferred Sports
+            Preferred Sports (Some Component will go here)
           </Text>
+          <Switch
+            style={{
+              paddingLeft: 100,
+              paddingRight: 100
+            }}
+            placeholder="Hello"
+            selectedValue={(this.state && football) || "a"}
+            onValueChange={value => {
+              this.setState({ football: value });
+            }}
+          >
+            He
+          </Switch>
           <TextInput
             style={{ padding: 50 }}
             placeholder="Phone Number (optional)"
             keyboardType="numeric"
             maxLength={10}
-            // selectedValue={(this.state && duration) || "a"}
-            // onChangeText={text => this.setState({ duration: text })}
+            selectedValue={(this.state && phone) || "a"}
+            onChangeText={text => this.setState({ phone: text })}
           />
           <TextInput
             style={{ padding: 50 }}
             placeholder="Email address (optional)"
-            // selectedValue={(this.state && duration) || "a"}
-            // onChangeText={text => this.setState({ duration: text })}
+            selectedValue={(this.state && email) || "a"}
+            onChangeText={text => this.setState({ email: text })}
           />
         </View>
       </ScrollView>
