414c4f4deaa4841c16334ac44d4b154f9f4670e2
Alec Martin <owner@Alecs-MacBook.local>
2018-11-14 18:22:18 -0600

    Added JSOn functions to request user info from Server

--- mobile/components/GameCard.js
+++ mobile/components/GameCard.js
@@ -4,6 +4,39 @@ import { View, Text, Image } from "react-native";
 
 import { Card, ListItem, Button, Icon } from "react-native-elements";
 
+const joinGame = () => {
+  const { navigation } = this.props;
+    try {
+      const request = await fetch(
+        "http://local-pickup-sports-manager.herokuapp.com/joinGame",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(userInfo),
+          json: true
+        }
+  //
+};
+
+const showInterest=()=>{
+  //
+  const { navigation } = this.props;
+    try {
+      const request = await fetch(
+        "http://local-pickup-sports-manager.herokuapp.com/showInterest",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(userInfo),
+          json: true
+        }
+
+}
+
 export default function GameCard(props) {
   // implemented with Text and Button as children
 
@@ -11,11 +44,9 @@ export default function GameCard(props) {
 
   return (
     <Card title={title}>
-      <Text style={{ marginBottom: 10 }}>Skill Level= </Text>
-      {skillLevel}
+      <Text style={{ marginBottom: 10 }}>Skill Level = {skillLevel}</Text>
 
-      <Text style={{ marginBottom: 10 }}>Time= </Text>
-      {duration}
+      <Text style={{ marginBottom: 10 }}>Time= {duration} </Text>
 
       <Text style={{ marginBottom: 10 }}>Join this game now!</Text>
 
@@ -28,6 +59,7 @@ export default function GameCard(props) {
           marginBottom: 0
         }}
         title="Join Game"
+        onClick={joinGame}
       />
       <Text style={{ marginBottom: 10 }} />
 
@@ -42,7 +74,21 @@ export default function GameCard(props) {
           marginBottom: 0
         }}
         title="Interested in Game"
+        onClick={showInterest}
       />
     </Card>
   );
 }
+
+const mapStateToProps = state => ({ userInfo });
+
+export default connect(mapStateToProps)(GameCard);
+
+GameCard.propTypes = {
+  token: PropTypes.string,
+  dispatch: PropTypes.func.isRequired
+};
+
+GameCard.defaultProps = {
+  token: null
+};
