aef60bf1784017b470985e9fdcb9795f520b369e
unknown <cjclark9551@gmail.com>
2018-11-15 15:34:48 -0600

    implemented adding game to DB

--- server/index.js
+++ server/index.js
@@ -1,67 +1,75 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
-app.use(bodyParser.json());
-const port = process.env.PORT || 3000;
+const app = express()
+app.use(bodyParser.json())
+const port = process.env.PORT || 3000
 
-const knex = require("knex")({
-  client: "mysql",
+const knex = require('knex')({
+  client: 'mysql',
   connection: {
-    host: "us-cdbr-iron-east-01.cleardb.net",
-    user: "b228d50d18d0dc",
-    password: "a7d38f2e",
-    database: "heroku_c7582aec8a53b97"
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'b228d50d18d0dc',
+    password: 'a7d38f2e',
+    database: 'heroku_c7582aec8a53b97'
   }
-});
+})
 
-app.get("/", (req, res) => res.send("Hello from API"));
+app.get('/', (req, res) => res.send('Hello from API'))
 
-app.post("/createGame", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-});
+app.post('/createGame', (req, res) => {
+  console.log(req.body)
+
+  knex('game').insert({
+    sport: req.body.sport,
+    duration: req.body.duration,
+    skillLevel: req.body.skill,
+    dateTime: req.body.chosenDate,
+    location: req.body.location,
+    minPlayers: req.body.minVal,
+    maxPlayers: req.body.maxVal
+  })
+
+  res.send(req.body)
+})
 
 /**
  * userId: string -> the user's db id
  * name: string -> players name
  * interested: boolean -> is the user interested or joining
  */
-app.post("/joinGame", (req, res) => {
-  const { userId, name, interested } = req.body;
-  console.log(`${userId} ${interested} ${name}`);
-  console.log(req.body);
-  res.send();
-});
+app.post('/joinGame', (req, res) => {
+  const { userId, name, interested } = req.body
+  console.log(`${userId} ${interested} ${name}`)
+  console.log(req.body)
+  res.send()
+})
 
 /**
  * We currently get the following fields from the request object
  * email, gender, facebook id, name
  */
-app.post("/loginUser", (req, res) => {
-  knex("user")
-    .where("email", req.body.email)
-    .first()
-    .then(user => {
-      if (!user) {
-        knex("user")
-          .insert({
-            email: req.body.email,
-            gender: req.body.gender,
-            username: req.body.username
-          })
-          .then(() => {
-            knex("user")
-              .where("email", req.body.email)
-              .first()
-              .then(addedUser => {
-                res.send(addedUser);
-              });
-          });
-      } else {
-        res.send(user);
-      }
-    });
-});
+app.post('/loginUser', (req, res) => {
+  knex('user').where('email', req.body.email).first().then(user => {
+    if (!user) {
+      knex('user')
+        .insert({
+          email: req.body.email,
+          gender: req.body.gender,
+          username: req.body.username
+        })
+        .then(() => {
+          knex('user')
+            .where('email', req.body.email)
+            .first()
+            .then(addedUser => {
+              res.send(addedUser)
+            })
+        })
+    } else {
+      res.send(user)
+    }
+  })
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
