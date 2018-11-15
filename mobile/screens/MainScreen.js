5f82e6dede82c169079c89409b522e1c7bc73ebe
Mustafa <amustafa246@yahoo.com>
2018-11-14 19:44:35 -0600

    basic layout of main screen

--- mobile/screens/MainScreen.js
+++ mobile/screens/MainScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, Platform } from "react-native";
+import { View, ScrollView, Platform } from "react-native";
 import { Button } from "react-native-elements";
+import { GameCard } from "../components/GameCard";
 
 class MainScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -26,10 +27,44 @@ class MainScreen extends Component {
     }
   });
 
+  componentDidMount() {
+    // TODO - Implement fetch
+  }
+
   render() {
+    const games = [
+      {
+        title: "Basketball",
+        skillLevel: "Beginner",
+        duration: "2 Hours"
+      },
+      {
+        title: "Football",
+        skillLevel: "Intermediate",
+        duration: "1 hour"
+      },
+      {
+        title: "Soccer",
+        skillLevel: "Expert",
+        duration: "30 minutes"
+      },
+      {
+        title: "Hockey",
+        skillLevel: "Beginner",
+        duration: "1 hour"
+      }
+    ];
     return (
       <View>
-        <Text>MainScreen - Hello world</Text>
+        <ScrollView>
+          {games.map(game => (
+            <GameCard
+              title={game.title}
+              skillLevel={game.skillLevel}
+              duration={game.duration}
+            />
+          ))}
+        </ScrollView>
       </View>
     );
   }
