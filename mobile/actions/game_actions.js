6ff43562c25831d4ed8c962c162813caadadabf1
Mustafa <amustafa246@yahoo.com>
2018-11-15 21:44:15 -0600

    added comment

--- mobile/actions/game_actions.js
+++ mobile/actions/game_actions.js
@@ -4,6 +4,7 @@ export const createGame = () => async dispatch => {};
 
 export const getGames = (location, preferences) => async dispatch => {
   try {
+    // TODO - Update link after merge to cloud
     const request = await fetch("http://192.168.1.2:3000/getGames", {
       method: "GET",
       headers: {
