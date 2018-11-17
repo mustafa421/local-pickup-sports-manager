e72b3bb1c70fc5d2ce492544eb7a66bc9b37c9b8
Starmi Samwell <byfan@wisc.edu>
2018-11-16 17:57:02 -0700

    Added unworking customer preferences

---
+++ mobile/screens/CustomizePreferences.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, FlatList, Text, ScrollView } from 'react-native';
+import { Constants } from 'expo';
+
+// or any pure javascript modules available in npm
+import { CheckBox } from 'react-native-elements';
+import { ListItem } from 'react-native-elements'
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import ActionButton from 'react-native-action-button';
+
+import axios from 'axios';
+import {AsyncStorage} from 'react-native';
+
+
+import "@expo/vector-icons";
+
+//list of all sports (should pull from database)
+const items = ['Badminton','Basketball','Football', 'Soccer', 'Softball' , 'Volleyball'];
+
+//default user prefernces
+const defaultPreferences = ['Football', 'Soccer'];
+var userPref = defaultPreferences;
+
+const storePreferences = async userPref => {
+  if (userPref == null) {
+    userPref = defaultPreferences;
+  }
+  try {
+    await AsyncStorage.setItem('user_Preferences', userPref);
+  } catch (error) {
+    // Error retrieving data
+    console.log(error.message);
+  }
+};
+
+const getPreferences = async () => {
+  let userPref = '';
+  if (userPref == null) {
+    userPref = defaultPreferences;
+  }
+  try {
+    userPref = await AsyncStorage.getItem('user_Preferences') || 'none';
+  } catch (error) {
+    // Error retrieving data
+    console.log(error.message);
+  }
+  return userPref;
+}
+export default class CustomizePreferences extends Component {
+
+
+  state = {
+    checked: userPref,
+  };
+
+  handleSubmit = () => {
+    console.log('SUBMITTED');
+  }
+
+  checkItem = item => {
+    const { checked } = this.state;
+    console.log(item, ' added');
+    if (!checked.includes(item)) {
+      this.setState({ checked: [...checked, item] });
+    } else {
+      this.setState({ checked: checked.filter(a => a !== item) });
+    }
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    let arr = [];
+    for (var key in this.state) {
+      if(this.state[key] === true) {
+        arr.push(key);
+      }
+    }
+    let data = {
+      check: arr.toString() 
+    };
+    axios.post('http://localhost:4000/checks/add', data)
+          .then(res => console.log(res.data));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          User Prefernces
+        </Text>
+        <Text style={styles.category}>
+          Sports
+        </Text>
+        <Text style={styles.description}>
+          Select any of the following that interest you
+        </Text>
+
+
+        
+        <FlatList
+          data={items}
+          extraData={this.state}
+          renderItem={({ item }) => (
+            <CheckBox
+              title={item}
+              checkedIcon='dot-circle-o'
+              uncheckedIcon='circle-o'
+              onPress={() => this.checkItem(item)}
+              checked={this.state.checked.includes(item)}
+            />
+          )}
+        />
+
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+    welcome: {
+    fontSize: 20,
+    paddingTop: Constants.statusBarHeight,
+    textAlign: 'center',
+    margin: 2,
+  },
+  category: {
+    fontSize: 16,
+    paddingTop: Constants.statusBarHeight,
+    textAlign: 'center',
+    margin: 2,
+  }
+});
