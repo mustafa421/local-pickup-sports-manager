297a32405bede1ded01140b0749bec0eeb214762
Alec Martin <owner@Alecs-MacBook.eau.wi.charter.com>
2018-11-16 21:42:50 -0600

    removed imports

--- mobile/__tests__/GameCard.test.js
+++ mobile/__tests__/GameCard.test.js
@@ -1,17 +1,10 @@
 import React from "react";
-import "react-native";
-import configureMockStore from "redux-mock-store";
-import thunk from "redux-thunk";
-import MockAsyncStorage from "mock-async-storage";
-import fetchMock from "fetch-mock";
-import { AsyncStorage } from "react-native";
-import { shallow } from "enzyme";
-import Button from "./Button";
+
 import renderer from "react-test-renderer";
 
 import GameCard from "../components/GameCard";
 
-// test join game (component is sending post requests)
+// test game card
 describe("GameCard tests", () => {
   test("snapshot", () => {
     const tree = renderer.create(<GameCard />).toJSON();
