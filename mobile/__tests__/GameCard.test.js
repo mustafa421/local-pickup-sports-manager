82289b78f62a2c170265e6a5e8a59648629fc4e6
Alec Martin <owner@Alecs-MacBook.eau.wi.charter.com>
2018-11-16 21:41:00 -0600

    Deleted unnecessary tests, added snapshot test

--- mobile/__tests__/GameCard.test.js
+++ mobile/__tests__/GameCard.test.js
@@ -8,50 +8,13 @@ import { AsyncStorage } from "react-native";
 import { shallow } from "enzyme";
 import Button from "./Button";
 import renderer from "react-test-renderer";
-import MainScreen from "../screens/MainScreen";
 
 import GameCard from "../components/GameCard";
 
 // test join game (component is sending post requests)
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
-
-const mock = () => {
-  const mockImpl = new MockAsyncStorage();
-  jest.mock("AsyncStorage", () => mockImpl);
-};
-
-mock();
-
-describe("Testing GameCard Components", () => {
-  test("Component sends post request", () => {
-    const customerMock = {
-      name: "Bob",
-      email: "bobsemail@gmail.com",
-      gender: "male"
-    };
-
-    fetchMock.postOnce(
-      "http://local-pickup-sports-manager.herokuapp.com/joinGame",
-      customerMock
-    );
-    const expectedActions = [
-      {
-        payload: { userAccountData: customerMock }
-      }
-    ];
-
-    // Check to see if we match
-    const store = mockStore();
-    expect(store.getActions()).toEqual(expectedActions);
-  });
-
-  test("Test click event", () => {
-    const mockCallBack = jest.fn();
-
-    const button = shallow(<Button onPress={mockCallBack}>Ok!</Button>);
-    button.find("button").simulate("click");
-
-    expect(mockCallBack.mock.calls.length).toEqual(1);
+describe("GameCard tests", () => {
+  test("snapshot", () => {
+    const tree = renderer.create(<GameCard />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
