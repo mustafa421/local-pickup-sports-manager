e8cac2edd234f8e0bf4a19f35c2bdb738605b475
Max <mglickman13@gmail.com>
2018-11-16 20:45:48 -0600

    Changes fixed

--- mobile/__tests__/createGameScreen.test.js
+++ mobile/__tests__/createGameScreen.test.js
@@ -2,11 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import CreateGameScreen from "../screens/CreateGameScreen";
 
-describe("sample test suite", () => {
-  test("test", () => {
-    expect(1 + 2).toBe(3);
-  });
-
+describe("createGameScreen tests", () => {
   test("snapshot", () => {
     const tree = renderer.create(<CreateGameScreen />).toJSON();
     expect(tree).toMatchSnapshot();
