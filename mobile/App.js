94cc37ea7001968aad42b5b26e1ba440e21fe528
Matthew Madden <thelightupmario@yahoo.com>
2018-11-16 21:43:10 -0600

    Removed Comments
    Removed Unneeded Comments

--- mobile/App.js
+++ mobile/App.js
@@ -47,15 +47,7 @@ export default class App extends React.Component {
          <MainNavigator />
        </Provider>
 
-       //For Testing the game component
-      //<Game
-      // location={"madison"}
-       // date={"Tomorrow"}
-      //  time={"12:00"}
-       // number_interested={3}
-       // number_going={4}
-       // difficulty={1}
-      //  />
+
     );
   }
 }
