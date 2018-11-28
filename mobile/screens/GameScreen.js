5f5fb92338a31ec44969d8ba1c32b4ecc7e5e886
Alec Martin <owner@Alecs-MacBook.eau.wi.charter.com>
2018-11-28 16:21:39 -0600

    "changed string values in GameScreen, also changed import of Game from "../components/game"

--- mobile/screens/GameScreen.js
+++ mobile/screens/GameScreen.js
@@ -1,29 +1,27 @@
 //Import Statements
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import Game from "../components/Game";
-
-
+import Game from "../components/game";
 
 class GameScreen extends Component {
-
-    render() {
-        return (
-          <View>
-            <Text></Text>
-            <Text></Text>
-            <Text>GameScreen - Hello world</Text>
-            <Game
-              location={"madison"}
-              date={"Tomorrow"}
-              time={"12:00"}
-              number_interested={3}
-              number_going={4}
-              difficulty={1}/>
-            <Text>Ahoy!</Text> 
-          </View>
-        );
-      }
+  render() {
+    return (
+      <View>
+        <Text />
+        <Text />
+        <Text>GameScreen - Hello world</Text>
+        <Game
+          location={"Madison"}
+          date={"11/30/18"}
+          time={"12:00"}
+          number_interested={3}
+          number_going={4}
+          difficulty={1}
+        />
+        <Text>Ahoy!</Text>
+      </View>
+    );
+  }
 }
 
 export default GameScreen;
