37d4204369becdd6b24c41da9cbde92ec7229146
Mustafa <amustafa246@yahoo.com>
2018-11-30 23:09:08 -0600

    added loading indicator

--- mobile/screens/AuthScreen.js
+++ mobile/screens/AuthScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View } from "react-native";
+import { View, ActivityIndicator, StyleSheet, Text } from "react-native";
 import { connect } from "react-redux";
 import { fbLogin, googleLogin } from "../actions/auth_actions";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    padding: 10
+  }
+});
 
 class AuthScreen extends React.Component {
   componentDidMount() {
@@ -30,7 +42,12 @@ class AuthScreen extends React.Component {
   }
 
   render() {
-    return <View />;
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <Text>Loading... </Text>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
   }
 }
 
