419a955e6da05cd8b5f185baa395c7de2e684434
Mustafa <amustafa246@yahoo.com>
2018-11-30 19:49:51 -0600

    replaced quotes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
-app.use(bodyParser.json())
-const port = process.env.PORT || 3000
+const app = express();
+app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 
-const knex = require('knex')({
-  client: 'mysql',
+const knex = require("knex")({
+  client: "mysql",
   connection: {
-    host: 'us-cdbr-iron-east-01.cleardb.net',
-    user: 'b228d50d18d0dc',
-    password: 'a7d38f2e',
-    database: 'heroku_c7582aec8a53b97'
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "b228d50d18d0dc",
+    password: "a7d38f2e",
+    database: "heroku_c7582aec8a53b97"
   }
-})
+});
 
-app.get('/', (req, res) => res.send('Hello from API'))
+app.get("/", (req, res) => res.send("Hello from API"));
 
 // TODO - Documentation for exact parameters expected as well as their types
-app.post('/createGame', (req, res) => {
+app.post("/createGame", (req, res) => {
   if (
     req.body.sport != null &&
     req.body.duration != null &&
@@ -30,7 +30,7 @@ app.post('/createGame', (req, res) => {
     req.body.latitude != null &&
     req.body.longitude != null
   ) {
-    knex('game')
+    knex("game")
       .insert({
         sport: req.body.sport,
         duration: req.body.duration,
@@ -42,12 +42,12 @@ app.post('/createGame', (req, res) => {
         latitude: req.body.latitude,
         longitude: req.body.longitude
       })
-      .then(() => {})
-    res.send(req.body)
+      .then(() => {});
+    res.send(req.body);
   } else {
-    res.send('One or more fields empty')
+    res.send("One or more fields empty");
   }
-})
+});
 
 /**
  * @param {*} latitude -> number
@@ -58,9 +58,9 @@ app.post('/createGame', (req, res) => {
  *
  *  i.e [ { duration, skill_level, ... }]
  */
-app.get('/getGames', (req, res) => {
+app.get("/getGames", (req, res) => {
   // Retrieve GET request URL parameters
-  const { latitude, longitude, preferences } = req.query
+  const { latitude, longitude, preferences } = req.query;
 
   // Add "HAVING distance < 25" to restrict games to under 25 miles
   knex
@@ -75,34 +75,34 @@ app.get('/getGames', (req, res) => {
     )
     .then(resp => res.send(resp[0].map(result => Object.assign({}, result))))
     .catch(err => {
-      res.status = 500
-      res.send(`Error getting games: ${err}`)
-    })
-})
+      res.status = 500;
+      res.send(`Error getting games: ${err}`);
+    });
+});
 
 /**
  * userId: string -> the user's db id
  * name: string -> players name
  * interested: boolean -> is the user interested or joining
  */
-app.post('/joinGame', (req, res) => {
-  const { userId, name, interested } = req.body
-  console.log(`${userId} ${interested} ${name}`)
-  console.log(req.body)
-  res.send()
-})
+app.post("/joinGame", (req, res) => {
+  const { userId, name, interested } = req.body;
+  console.log(`${userId} ${interested} ${name}`);
+  console.log(req.body);
+  res.send();
+});
 
 /**
  * We currently get the following fields from the request object
  * email, gender, facebook id/google id, name
  */
-app.post('/loginUser', (req, res) => {
-  knex('user')
-    .where('email', req.body.email)
+app.post("/loginUser", (req, res) => {
+  knex("user")
+    .where("email", req.body.email)
     .first()
     .then(user => {
       if (!user) {
-        knex('user')
+        knex("user")
           .insert({
             vendorID: req.body.id,
             email: req.body.email,
@@ -110,45 +110,45 @@ app.post('/loginUser', (req, res) => {
             username: req.body.username
           })
           .then(() => {
-            knex('user')
-              .where('email', req.body.email)
+            knex("user")
+              .where("email", req.body.email)
               .first()
               .then(addedUser => {
-                res.send(addedUser)
-              })
-          })
+                res.send(addedUser);
+              });
+          });
       } else {
-        res.send(user)
+        res.send(user);
       }
-    })
-})
+    });
+});
 
-app.get('/getUserByID', (req, res) => {
-  knex('user')
-    .where('userID', req.body.id)
+app.get("/getUserByID", (req, res) => {
+  knex("user")
+    .where("userID", req.body.id)
     .first()
     .then(user => {
       if (user) {
-        res.send(user)
+        res.send(user);
       } else {
-        res.send('user does not exist')
+        res.send("user does not exist");
       }
-    })
-})
+    });
+});
 
-app.get('/getGameByID', (req, res) => {
-  knex('game')
-    .where('gameID', req.body.id)
+app.get("/getGameByID", (req, res) => {
+  knex("game")
+    .where("gameID", req.body.id)
     .first()
     .then(game => {
       if (game) {
-        res.send(game)
+        res.send(game);
       } else {
-        res.send('game does not exist')
+        res.send("game does not exist");
       }
-    })
-})
+    });
+});
 
 app.listen(port, () =>
   console.log(`Local Pickup Sports Manager backend listening on port ${port}!`)
-)
+);
